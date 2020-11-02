734490cfc76defb5c96d776ff256d212ee655da4
Corneille Tshibasu <corneilletshibasu@gmail.com>
2020-11-02 15:39:34 +0000

    Updates

--- create-server/routes.js
+++ create-server/routes.js
@@ -27,7 +27,7 @@ const requestHandler = (req, res) => {
                         const parsedBody = Buffer.concat(body).toString();
                         console.log(parsedBody);
                         const message = parsedBody.split('=')[1]; // contains message text
-                        fs.writeFile('message.txt', message, (err) => {
+                        fs.writeFile('./create-server/message.txt', message, (err) => {
                                 res.statusCode = 302;
                                 res.setHeader('Location', '/');
                                 return res.end();
